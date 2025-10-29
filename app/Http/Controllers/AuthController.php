3ce16b11d126a1332d86f87ef78c136d4a0cfc19
Ole Hendrix <160501406+OleHendrix@users.noreply.github.com>
2025-10-29 15:44:01 +0100

    api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+  public function login(Request $request)
+  {
+    $validation_rules = [
+      'email' => 'required|email',
+      'password' => 'required|string|min:8',
+    ];
+    $validated = $request->validate($validation_rules);
+
+    if (!Auth::attempt($validated)) {
+      throw ValidationException::withMessages([
+        'email' => ['The provided credentials are incorrect.'],
+      ]);
+    }
+
+    $request->session()->regenerate();
+    return response()->json(Auth::user());
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('users', UserController::class);
+// Route::apiResource('/user', UserController::class);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
